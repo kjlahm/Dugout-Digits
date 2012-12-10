dd65350633f4f426ed616b7860c2334920496b9f
Ken Lahm <kjlahm@gmail.com>
2012-12-10 10:25:47 -0600

    Added permission class. Didn't get committed with last commit.

---
+++ Source/DugoutDigits/DugoutDigits/Objects/Permissions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DugoutDigits.Objects
+{
+    public class Permissions
+    {
+        public Boolean coachEnabled { get; set; }
+
+        public Permissions(bool coach)
+        {
+            coachEnabled = coach;
+        }
+
+        public Permissions() : this(false) { }
+    }
+}
